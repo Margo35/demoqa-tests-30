d8fd571058208916c420a63ed72a2951400c050a
Puldas Margo <puldas_margarita@mail.ru>
2024-09-12 19:34:35 +0600

    added cat.jpg in resourses and edited TextBoxTests.java

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -5,44 +5,57 @@ import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class TextBoxTests {
 
-    File file = new File("C:\\Users\\pulda\\Desktop\\qa_guru\\cat.jpg");
-
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.holdBrowserOpen = true;
+        Configuration.pageLoadStrategy = "eager";
     }
 
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
-        //sleep(30000);
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
         $("#firstName").setValue("Adam");
         $("#lastName").setValue("Brown");
         $("#userEmail").setValue("brown.adam@gmail.com");
         $("[for='gender-radio-1']").click();
-        $("#userNumber").setValue("79084558790");
+        $("#userNumber").setValue("7908455879");
         $("#dateOfBirthInput").click();
         $("#subjectsInput").click();
         $("#subjectsInput").setValue("Comp").pressEnter();
-        Selenide.executeJavaScript("document.getElementById('dateOfBirthInput').value='';");
+        $("#dateOfBirthInput").click();
+        Selenide.executeJavaScript("document.getElementById('dateOfBirthInput').value=''");
+        sleep(7000);
         $("#dateOfBirthInput").setValue("01 Jul 1990").pressEnter();
+        sleep(7000);
         $("[for='hobbies-checkbox-2']").click();
         $("[for='hobbies-checkbox-3']").click();
-        $("#uploadPicture").uploadFile(file);
+        $("#uploadPicture").uploadFromClasspath("cat.jpg");
         $("#currentAddress").setValue("75 PARK PLACE 8TH FLOOR NEW YORK");
         $("#state").scrollTo().click();
         $("#react-select-3-input").setValue("Har").pressEnter();
         $("#city").scrollTo().click();
         $("#react-select-4-input").setValue("Karn").pressEnter();
         $("#submit").scrollTo().click();
+        $(".modal-content").should(appear).shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Adam Brown"), text("brown.adam@gmail.com"),
+                text("Male"), text("7908455879"), text("01 July,1990"), text("Computer Science"), text("Reading, Music"),
+                text("cat.jpg"), text("75 PARK PLACE 8TH FLOOR NEW YORK"), text("Haryana Karnal"));
+
+        //, text("brown.adam@gmail.com"),
+        //                text("7908455879"), text("12 July,202401"), text("Computer Science"), text("Reading, Music"),
+        //                text("cat.jpg"), text("75 PARK PLACE 8TH FLOOR NEW YORK"), text("Haryana Karnal")
 
     }
 
